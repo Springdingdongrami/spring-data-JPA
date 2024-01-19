75808d671bc5c561d95d93d5300e7d6b8ca24fc2
kdozlo <eodud5730@naver.com>
2024-01-19 10:39:13 +0900

    Feat : 섹션 3. 공통 인터페이스 기능

--- kimdaeyoung/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ kimdaeyoung/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -34,4 +34,31 @@ class MemberJpaRepositoryTest {
         Assertions.assertThat(findMember).isEqualTo(member1);
     }
 
+    @Test
+    public void basicCRUD() {
+
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        //단건 조회 검증
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        Long count = memberJpaRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        Long deletedCount = memberJpaRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+
+    }
 }
--- kimdaeyoung/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ kimdaeyoung/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -32,4 +32,32 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member1.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member1);
     }
+
+    @Test
+    public void basicCRUD() {
+
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        //단건 조회 검증
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        Long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        Long deletedCount = memberRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+
+    }
 }
